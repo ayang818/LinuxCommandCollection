fc20b27b9dc1d3454908a7ff0d6a3a3afe6995fe
ayang818 <ayang818@qq.com>
2019-09-19 00:14:15 +0800

    磁盘相关命令

--- disk/disk.bash
+++ disk/disk.bash
@@ -2,4 +2,22 @@
 fdisk -l
 
 # 硬盘分区
-fdisk 
+fdisk /dev/磁盘名称
+
+# linux分区格式化
+mkfs.ext3 /dev/分区名
+
+# directory挂载分区
+mount /dev/分区名 /directory
+
+# 查看数据卷
+df -h
+
+# 取消directory挂载分区
+umount /dev/分区
+
+# 检查文件分区
+e2fsck -f /dev/分区名
+
+# 变更文件系统大小
+resize2fs /dev/分区名
