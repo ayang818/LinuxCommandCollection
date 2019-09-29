f6425747ec9368edb65e880dd6b53984e067850e
ayang818 <ayang818@qq.com>
2019-09-29 12:08:33 +0800

    alter to create index

--- sql(mysql)/index/create.sql
+++ sql(mysql)/index/create.sql
@@ -1,5 +1,8 @@
 -- create index
 create index idx_name on table_name(column1, column2);
 
+-- create index
+alter table table_name add index index_name(column1, column2);
+
 -- drop index
 drop index idx_name on table_name;
