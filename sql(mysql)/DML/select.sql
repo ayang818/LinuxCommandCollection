0c0f5f3f1d8135cb29713d896c1705490894bd9f
ayang818 <ayang818@qq.com>
2019-11-18 14:19:59 +0800

    add subquery example and joinquery example

--- sql(mysql)/DML/select.sql
+++ sql(mysql)/DML/select.sql
@@ -24,6 +24,24 @@ select count(*) num, user from user_info group by user with rollup having count(
 select a.user, b.get_like from user_info a, message_info b where a.id=b.user_id;
 select a.user, b.get_like from user_info a inner join message_info b on a.id=b.user_id;
 
+-- 举例
+-- Employee 表包含所有员工，他们的经理也属于员工。每个员工都有一个 Id，此外还有一列对应员工的经理的 Id。
+-- +----+-------+--------+-----------+
+-- | Id | Name  | Salary | ManagerId |
+-- +----+-------+--------+-----------+
+-- | 1  | Joe   | 70000  | 3         |
+-- | 2  | Henry | 80000  | 4         |
+-- | 3  | Sam   | 60000  | NULL      |
+-- | 4  | Max   | 90000  | NULL      |
+-- +----+-------+--------+-----------+
+-- 给定 Employee 表，编写一个 SQL 查询，该查询可以获取收入超过他们经理的员工的姓名。在上面的表格中，Joe 是唯一一个收入超过他的经理的员工。
+
+-- 使用子查询(对外层查询结果进行遍历时进行子查询)
+select Name as Employee from Employee a where a.Salary > (select Salary from Employee b where b.id = a.ManagerId);
+
+-- 使用内联查询
+select a.Name Employee from Employee  a, Employee b where a.ManagerId = b.id and a.Salary >= b.Salary; 
+
 -- 左连接查询
 select a.user,b.get_like from user_info a left join message_info b on a.id=b.user_id;
 
