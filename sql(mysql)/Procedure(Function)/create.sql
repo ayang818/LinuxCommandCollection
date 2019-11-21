853dcdb184dbf50fd79aa873decf0449fa83866e
ayang818 <ayang818@qq.com>
2019-11-21 18:52:02 +0800

    create insert procedure

--- sql(mysql)/Procedure(Function)/create.sql
+++ sql(mysql)/Procedure(Function)/create.sql
@@ -11,3 +11,16 @@ set i = i+1;
 end while;
 return res_str;
 end $$
+
+-- 创建插入数据的存储过程
+create procedure insert_user_test(in start int(10), in max_num int(10))
+begin 
+    declare i int default 0;
+    set autocommit = 0;
+    repeat
+        set i = i + 1;
+        insert into tbl_user values((start+i), rand_str(8), concat(rand_str(6), '@ayang818.com'), 1+floor(rand()*100), 3 ,now());
+        until i = max_num
+    end repeat;
+    commit;
+end $$
